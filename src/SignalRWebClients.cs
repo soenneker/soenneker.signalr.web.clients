5f7836b2c9aaab0f22897ff961f02564e756ebee
Jake Soenneker <jake@soenneker.com>
2025-12-19 20:33:29 -0600

    Deps

--- src/SignalRWebClients.cs
+++ src/SignalRWebClients.cs
@@ -4,36 +4,27 @@ using Soenneker.SignalR.Web.Client;
 using Soenneker.SignalR.Web.Client.Options;
 using System.Threading.Tasks;
 using System.Threading;
-using System;
 
 namespace Soenneker.SignalR.Web.Clients;
 
 /// <inheritdoc cref="ISignalRWebClients"/>
 public sealed class SignalRWebClients: ISignalRWebClients
 {
-    private readonly SingletonDictionary<SignalRWebClient> _clients;
+    private readonly SingletonDictionary<SignalRWebClient, SignalRWebClientOptions> _clients;
 
     public SignalRWebClients()
     {
-        _clients = new SingletonDictionary<SignalRWebClient>(objects =>
-        {
-            if (objects.Length == 0)
-                throw new Exception("SignalR options are required on first Get() call");
-
-            var options = (SignalRWebClientOptions)objects[0];
-
-            return new SignalRWebClient(options);
-        });
+        _clients = new SingletonDictionary<SignalRWebClient, SignalRWebClientOptions>(options => new SignalRWebClient(options));
     }
 
     public ValueTask<SignalRWebClient> Get(string id, SignalRWebClientOptions? options = null, CancellationToken cancellationToken = default)
     {
-        return _clients.Get(id, cancellationToken, options!);
+        return _clients.Get(id, options, cancellationToken);
     }
 
     public SignalRWebClient GetSync(string id, SignalRWebClientOptions? options = null, CancellationToken cancellationToken = default)
     {
-        return _clients.GetSync(id, cancellationToken, options!);
+        return _clients.GetSync(id, options, cancellationToken);
     }
 
     public ValueTask Remove(string id)
